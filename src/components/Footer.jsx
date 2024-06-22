e09aefa6900ac80e59adc522404ad1db614deb96
umesh2407 <choudharyumesh986@gmail.com>
2024-06-22 20:06:27 +0530

    added user

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -73,7 +73,7 @@ export default function Footer() {
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2023
                         <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                            Umeshchoudhary
                         </a>
                         . All Rights Reserved.
                     </span>
